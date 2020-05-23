1f3f92740ba13cd57a195e202a6cca1b9626b4dd
Tshoms <tshomsjordy@gmail.com>
2020-05-23 04:11:04 +0200

    14rd commit

--- javascript/orinoco.js
+++ javascript/orinoco.js
@@ -1,5 +1,25 @@
 ///  start javascript ///
 
-alert("bienvenu chez Orinoco");
+/// ---alert("bienvenu chez Orinoco");
 
-//prompt("ça va ?");
+///---prompt("ça va ?");
+
+const url = 'http://localhost:3000/api/teddies/';
+
+/// créer une requète
+
+let requete = new XMLHttpRequest(); // créer un objet
+requete.open('GET', url); // GET ou POST , deuxième param: l'url
+requete.responseType = 'json'; // nous attendons du json
+requete.send(); // nous envoyons nottre requete
+
+// des qu'on reçoit une réponse , cette fonction est éxecuter
+
+requete.onload = function() {
+if (requete.readyState === XMLHttpRequest.DONE) {
+  if (requete.status === 200) {
+    let reponse = requete.response; // on stock la réponse
+    console.log(reponse);
+     }
+   }
+ }
--- javascript/produit.js
+++ javascript/produit.js
@@ -1,3 +1,13 @@
 // fichier .js / produit
 
-alert("tu es sur le point de m'adopter ;)");
+//alert("tu es sur le point de m'adopter ;)");
+
+//---------------------- tableau
+
+// let tableau =  [
+// ['norbert',' Arnold','lenny and carl','gustav','Garfunkel']
+// ['2900',' 3900','4900','5900','6900']
+
+//];
+
+//console.log(tableau[0]);
